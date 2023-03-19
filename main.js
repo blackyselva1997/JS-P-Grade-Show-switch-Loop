0ff406f1a8a01256e274f5f2a80fec2036cea9d4
blackyselva <selvam17196@gmail.com>
2023-03-19 20:55:57 +0530

    Adding.

---
+++ main.js
@@ -0,0 +1,20 @@
+let mark = String(prompt("Enter a Grade"));
+
+switch (mark){
+    case 'A':
+        document.write("Outstanding");
+        break;
+    case 'B':
+        document.write("Excellent");
+        break;
+    case 'C':
+        document.write("Well done");
+        break;
+    case 'D':
+        document.write("Passed");
+        break;
+    case 'F':
+        document.write("Failed");
+        break;
+        default:
+}
